25ceb2ecca3cfbd7de36a8a1ab4088040be08c31
auriel grant <aurgrant@gmail.com>
2023-03-24 16:23:04 -0500

    jquery selectors exercise

--- js/jqueryexercise.js
+++ js/jqueryexercise.js
@@ -4,3 +4,23 @@
 $(function(){
     alert( 'The DOM has finished loading !')
 });
+
+// let first = $('#auriel').html();
+// alert(first);
+
+// let first = $('#aurielPara').html();
+// alert(first);
+
+// $('.codeup').css('border', '1px solid red');
+
+// $('li').css('font-size', '20px');
+// $('h1').css('background', 'peru');
+// $('p').css('background', 'pink');
+// $('li').css('background', 'salmon');
+
+let last =$('h1').html();
+alert(last);
+
+
+
+$('p,li,h1').css("background", 'yellow');
